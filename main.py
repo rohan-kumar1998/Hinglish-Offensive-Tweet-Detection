ce7601604ee12d490f40dfc8888ea4f82be823d7
rohan-kumar1998 <46169768+rohan-kumar1998@users.noreply.github.com>
2019-10-11 16:02:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ from sklearn.metrics import confusion_matrix
 import pandas as pd
 
 from data_loader import loader 
+from data_loader_map import loader_map
 from trainer import trainer 
-from models import RNNModel, CNNmodel, RNN, CNN, RNN_CNNmodel
+from models import RNNModel, CNNmodel, CNN, RNN, RNN_CNNmodel
 
 torch.manual_seed(1234)
 torch.backends.cudnn.deterministic = True
@@ -64,6 +65,48 @@ rnn = RNN(vocab_size, 50, 100, weights_matrix, dropout = dropout).cuda()
 
 model = RNN_CNNmodel(cnn, rnn, 3, dropout).cuda()
 optimizer = optim.Adam(model.parameters(), lr= 1e-3)
+criterion = nn.BCEWithLogitsLoss().cuda()
+
+trainer(model, train_data, val_data,test_data, optimizer, criterion, 20)
+
+src = "./HOT_cleaned.csv"
+emb_src = "./glove.6B.50d.txt"
+prof_src = "./Hinglish_Profanity_List.csv"
+train_data, val_data, test_data, vocab, weights_matrix = loader_map(src, emb_src, prof_src, 50 , 50)
+
+output_dim  =  3
+emb_dim = 50 
+hid_dim = 100 
+
+model = RNNModel(output_dim, emb_dim , hid_dim , weights_matrix,dropout = 0.5).cuda()
+optimizer = optim.Adam(model.parameters(), lr= 1e-3)
+criterion = nn.BCEWithLogitsLoss().cuda()
+
+trainer(model, train_data, val_data,test_data, optimizer, criterion, 20)
+
+embedding_dim = 50
+n_filters = 100
+filter_sizes = [3,4,5]
+output_dim = 3
+dropout = 0.5
+
+model = CNNmodel(embedding_dim, n_filters , filter_sizes, output_dim, weights_matrix ,dropout).cuda()
+optimizer = optim.Adam(model.parameters(), lr= 1e-2)
+criterion = nn.BCEWithLogitsLoss().cuda()
+
+trainer(model, train_data, val_data,test_data, optimizer, criterion, 20)
+
+embedding_dim = 50
+n_filters = 100
+filter_sizes = [3,4,5]
+output_dim = 3
+dropout = 0.3
+
+cnn = CNN(embedding_dim, n_filters , filter_sizes, output_dim,weights_matrix, dropout).cuda()
+rnn = RNN(vocab_size, 50, 100, weights_matrix, dropout = dropout).cuda()
+
+model = RNN_CNNmodel(cnn, rnn, 3, dropout).cuda()
+optimizer = optim.Adam(model.parameters(), lr= 1e-2)
 criterion = nn.BCEWithLogitsLoss().cuda()
 
 trainer(model, train_data, val_data,test_data, optimizer, criterion, 20)
